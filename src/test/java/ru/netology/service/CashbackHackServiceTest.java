06ccf356c0ab0362b8585e909e572cacab6d4ecb
Natalia <nika_pozitivka@mail.ru>
2022-07-26 22:50:27 +0300

    testng

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,36 @@
+package ru.netology.service;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldRemain400() {
+        int actual = service.remain(1600);
+        int expected = 400;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldRemain700() {
+        int actual = service.remain(3300);
+        int expected = 700;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldRemain() {
+        int actual = service.remain(0);
+        int expected = 0;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldRemain0() {
+        int actual = service.remain(1000);
+        int expected = 0;
+        Assert.assertEquals(actual, expected);
+    }
+}
